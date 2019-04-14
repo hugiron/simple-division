136a23a6a1a9067415368501733da868ea585de8
Vladislav Bakaev <vb@marketguard.me>
2019-04-15 03:42:20 +0400

    Fix

--- div.c
+++ div.c
@@ -4,24 +4,15 @@
 #include <stdbool.h>
 #include <string.h>
 
-char* resize(char* array, int size) {
-	return (char*)realloc(array, size * sizeof(char));
-}
-
 char* simple_division(int a, int b, int n) {
-	int step = n;
-	int size = step;
-	char* result = (char*)malloc(size * sizeof(char));
+	int size = sizeof(char) * (sizeof(int) + n);
+	char* result = (char*)malloc(size);
 	snprintf(result, sizeof(result), "%d", a / b);
 	a = abs(a) % abs(b);
 	bool point_used = false;
 	bool extend_used = false;
 	int current_index = strlen(result);
 	while (n > 0 && a != 0) {
-		if (current_index == (size - 1)) {
-			size += step;
-			result = resize(result, size);
-		}
 		if (a >= b) {
 			result[current_index++] = '0' + (a / b);
 			a %= b;
